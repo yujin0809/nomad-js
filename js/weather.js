b315b61efae6d46ae178f817614cf2bd4ad7becd
yujin0809 <87366966+yujin0809@users.noreply.github.com>
2023-12-10 20:38:59 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "62ad5ba9ff183707e7605ffe20aef255";
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log('You live in', lat, lon);
+    
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector("#weather .weather");
